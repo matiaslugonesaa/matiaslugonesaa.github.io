4a7c11ea26d521a4ff75e71ad558985cfc1879c8
matiaslugonesa <127349090+matiaslugonesa@users.noreply.github.com>
2023-03-08 17:37:19 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+document.getElementById('boton_ocultar').addEventListener('click', function() {
+document.getElementById("contacto").style.display ='none';
+});
+document.getElementById('boton_mostrar').addEventListener('click', function() {
+document.getElementById("contacto").style.display = 'block';
+    });
